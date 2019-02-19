b5289aa427c059607a4c889f833266c6a2868cf8
Brett Andrews <brett.h.andrews@gmail.com>
2019-02-18 23:10:13 -0500

    tries directly importing marvin.extern

--- python/marvin/contrib/vacs/base.py
+++ python/marvin/contrib/vacs/base.py
@@ -6,7 +6,7 @@
 # @Author: Brian Cherinka
 # @Date:   2018-06-21 17:01:09
 # @Last modified by:   andrews
-# @Last modified time: 2019-02-18 22:02:52
+# @Last modified time: 2019-02-18 23:02:01
 
 from __future__ import absolute_import, division, print_function
 
@@ -24,8 +24,7 @@ from marvin.core.exceptions import MarvinError
 try:
     import sdss_access.sync
 except ModuleNotFoundError as e:
-    import sys
-    print(sys.path)
+    import marvin.extern
     raise
 
 __ALL__ = ['VACContainer', 'VACMixIn']
